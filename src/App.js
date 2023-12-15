d02151bcf137a24e4d5a1f71a4e19bbaa8471776
khizarmd <mohommadkhizar2@gmail.com>
2023-12-15 17:27:24 +0530

    product name changed

--- src/App.js
+++ src/App.js
@@ -10,24 +10,24 @@ const App = () => {
   const products = [
     {
       id: 'p1',
-      title: 'Nirma',
+      title: 'Product-1',
       amount: 100,
       date: new Date(2021, 8, 10),
     },
     { 
       id: 'p2', 
-      title: 'Surf Excel', 
+      title: 'Product-2', 
       amount: 200, 
       date: new Date(2021, 2, 2) },
     {
       id: 'p3',
-      title: 'Tide',
+      title: 'Product-3',
       amount: 130,
       date: new Date(2021, 12, 28),
     },
     {
       id: 'p4',
-      title: 'Maggi',
+      title: 'Product-4',
       amount: 450,
       date: new Date(2021, 5, 5),
     },
